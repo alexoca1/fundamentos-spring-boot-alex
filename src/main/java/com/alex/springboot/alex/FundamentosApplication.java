62e434c1b51a64064e1f547496c290dacf29a781
alexoca1 <alexanderocaher@gmail.com>
2023-03-02 10:56:00 +0100

    primer commit

---
+++ src/main/java/com/alex/springboot/alex/FundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.alex.springboot.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/springboot/alex/AlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.springboot.alex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
